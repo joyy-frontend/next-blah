2ece41834209378a32a2eb23d679e0f26b42b4ba
rhkk7157 <rhkk7157@gmail.com>
2023-06-18 03:38:03 +0900

    메인페이지 View 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
 import { NextPage } from 'next';
+import { Box, Center, Flex, Heading } from '@chakra-ui/react';
 import { ServiceLayout } from '@/components/service_layout';
+import { GoogleLoginButton } from '@/components/google_login_button';
 
 // eslint-disable-next-line react/function-component-definition
-const IndexPage: NextPage = () => <ServiceLayout title="test">test</ServiceLayout>;
+const IndexPage: NextPage = () => (
+  <ServiceLayout title="test">
+    <Box maxW="md" mx="auto">
+      <img src="/main_logo.svg" alt="메인 로고" />
+      <Flex justify="center">
+        <Heading>#BlahBlah</Heading>
+      </Flex>
+    </Box>
+    <Center mt="20">
+      <GoogleLoginButton />
+    </Center>
+  </ServiceLayout>
+);
 export default IndexPage;
