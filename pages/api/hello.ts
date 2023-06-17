afaefc4ff0408b7667f20e89f7ea37c4e43a35c7
rhkk7157 <rhkk7157@gmail.com>
2023-06-18 01:10:41 +0900

    quotes 변경 (Double -> Single)

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from "next";
-import FirebaseAdmin from "@/models/firebase_admin";
+import { NextApiRequest, NextApiResponse } from 'next';
+import FirebaseAdmin from '@/models/firebase_admin';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   FirebaseAdmin.getInstance().Firebase.collection('test');
