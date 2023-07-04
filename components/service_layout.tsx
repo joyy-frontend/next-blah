353569eb6bc73154bd2651f7308e7672796e977c
rhkk7157 <rhkk7157@gmail.com>
2023-07-04 17:50:54 +0900

    메인 뷰 페이지 수정

--- components/service_layout.tsx
+++ components/service_layout.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/function-component-definition */
+import { Box, BoxProps } from '@chakra-ui/react';
 import Head from 'next/head';
+import GNB from './GNB';
 
 interface Props {
   title: string;
@@ -6,13 +9,15 @@ interface Props {
 }
 
 // 각페이지를 구성할때 사용하게 됨.
-export function ServiceLayout({ title = 'blah x2', children }: Props) {
+export const ServiceLayout: React.FC<Props & BoxProps> = function ({ title = 'blah x2', children, ...boxProps }) {
   return (
-    <div>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Box {...boxProps}>
       <Head>
         <title>{title}</title>
       </Head>
+      <GNB />
       {children}
-    </div>
+    </Box>
   );
-}
+};
